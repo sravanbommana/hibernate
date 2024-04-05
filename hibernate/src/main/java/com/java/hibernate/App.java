99736a23d6212553f444e42a0e0d4c1243c862cb
Sravan Kumar <bommana@iloguest.org>
2024-04-05 21:06:13 +0530

    added table creation changes

--- hibernate/src/main/java/com/java/hibernate/App.java
+++ hibernate/src/main/java/com/java/hibernate/App.java
@@ -1,6 +1,8 @@
 package com.java.hibernate;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class App 
@@ -12,5 +14,17 @@ public class App
         cfg.configure("hibernate.cfg.xml");
         SessionFactory factory = cfg.buildSessionFactory();
         System.out.println(factory);
+        
+        Student student = new Student();
+        student.setId(10);
+        student.setCity("Hyderabad");
+        student.setName("Ramu");
+        
+        System.out.println(student);
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(student);
+        tx.commit();
+        session.close();
     }
 }
