7d95bf7c64abc348ce0e022cd7a2b2d3b6368d0f
Sravan Kumar <bommana@iloguest.org>
2024-04-09 14:20:19 +0530

    added many to many

--- hibernate/src/main/java/com/java/hibernate/App.java
+++ hibernate/src/main/java/com/java/hibernate/App.java
@@ -21,31 +21,46 @@ public class App
     	SessionFactory factory = cfg.buildSessionFactory();
     	Session session = factory.openSession();
     	Transaction tx  = session.beginTransaction();
-    	// Creation part
-    	/*
-    	Employee emp = new Employee();
-    	emp.setEmpId(10);
-    	emp.setName("Tim");
-    	BankAccount account1 = new BankAccount();
-    	account1.setAccountNo("4565788979");
-    	account1.setBranchName("Nagpur");
-    	account1.setEmployee(emp);
-    	BankAccount account2 = new BankAccount();
-    	account2.setAccountNo("6878898844");
-    	account2.setBranchName("Hyderabad");
-    	List<BankAccount> list= new ArrayList<BankAccount>();
-    	list.add(account1);
-    	list.add(account2);
-    	account2.setEmployee(emp);
-    	emp.setAccount(list);
-    	session.save(emp);
-    	session.save(account1);
-    	session.save(account2); */
-    	// Data Reading Part
-    	Employee emp = (Employee)session.get(Employee.class, 10);
-    	System.out.println(emp.getName());
-    	List<BankAccount> accountList = emp.getAccount();
-    	accountList.stream().forEach(act -> System.out.println(act.getAccountNo()));
+    	EmployeeOne emp1 = new EmployeeOne();
+    	EmployeeOne emp2 = new EmployeeOne();
+    	
+    	Project project1 = new Project();
+    	Project project2 = new Project();
+    	
+    	emp1.setId(21);
+    	emp1.setName("John");
+    	emp2.setId(11);
+    	emp2.setName("Doe");
+    	
+    	project1.setPid(1234);
+    	project1.setProjectName("Library Management");
+    	project2.setPid(5678);
+    	project2.setProjectName("Banking Management");
+    	
+    	List<EmployeeOne> empList1 = new ArrayList();
+    	List<EmployeeOne> empList2 = new ArrayList();
+    	
+    	empList1.add(emp1);
+    	empList1.add(emp2);
+    	empList2.add(emp2);
+    	
+    	List<Project> projectList1 = new ArrayList();
+    	List<Project> projectList2 = new ArrayList();
+    	
+    	projectList1.add(project1);
+    	projectList1.add(project2);
+    	projectList2.add(project2);
+    	
+    	emp1.setProject(projectList1);
+    	emp2.setProject(projectList2);
+    	project1.setEmployee(empList1);
+    	project2.setEmployee(empList2);
+    	
+    	session.save(emp1);
+    	session.save(emp2);
+    	session.save(project1);
+    	session.save(project2);
+    	
     	tx.commit();
     	session.close();
     	factory.close();
